57799fef370651dd33183c7121a9cbdc1b8c3462
marpme <marvin.piekarek5@gmail.com>
2018-06-04 22:01:16 +0200

    correcting types

--- src/Transaction.ts
+++ src/Transaction.ts
@@ -1,20 +1,20 @@
 export interface Transaction {
-	/**
-	 * The transaction id
-	 */
-	txid: string
-	/**
-	 * the output number
-	 */
-	vout?: string
-	account: String
-	address: String
-	category: String
-	amount: Number
-	confirmations: Number
-	blockhash: String
-	blockindex: Number
-	blocktime: Number
-	time: Number
-	timereceived: Number
+  /**
+   * The transaction id
+   */
+  txid: string
+  /**
+   * the output number
+   */
+  vout?: string
+  account: string
+  address: string
+  category: string
+  amount: number
+  confirmations: number
+  blockhash: string
+  blockindex: number
+  blocktime: number
+  time: number
+  timereceived: number
 }
